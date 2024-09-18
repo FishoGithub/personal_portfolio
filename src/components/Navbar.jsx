169d818d31ad7afa478ee6e9607153344b4aa3da
mihir majgaonkar <mihir.majgaonkar@gmail.com>
2024-09-18 10:08:52 -0700

    changed shadows on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className='fixed top-0 left-0 right-0 z-10 '>
-        <div className='drop-shadow-xl'>
+        <div className='drop-shadow-2xl'>
             <nav className=" mb-10 py-5 mx-96 items-center bg-gray-200 opacity-95 my-10 rounded-full border-slate-300 border-2 border-solid border-opacity-50 ">
                 <div className="position: relative flex  mx-20 text-center items-center ">
                     {/* <a href='.'><img src='/src/assets/mihir_logo.png' className='max-w-14 opacity-100'/></a> */}
