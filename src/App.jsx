ffe79e7decb3b5f659a1889d35fcba3587a16fdc
mihir majgaonkar <mihir.majgaonkar@gmail.com>
2024-09-30 10:09:06 -0700

    added smooth scrolling to whole page. working on parallax

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,9 @@ import Footer from './components/Footer'
 
 const App = () => {
   return (
-    <div className="absolute top-0 z-[-2] h-screen w-screen bg-white 
-    bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)]">
+    <div className='scroll-smooth'>
+      <div className="absolute top-0 z-[-2] h-screen w-screen bg-white 
+      bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)]">
 
       {/* navbar */}
         <Navbar/>
@@ -17,6 +18,7 @@ const App = () => {
         <AboutMe/>
         <Experiences/>
         <Footer/>
+    </div>
     </div>
   )
 }
